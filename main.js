1a0a095a9f0cd87dea48e2f39e9134719f7ecaec
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-07-29 15:50:29 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,85 @@
+"use strict";
+
+// Do not change any of the function names
+
+// 1
+function returnFirst(arr) {
+    // return the first item from the array
+  }
+  // 2
+  function returnLast(arr) {
+    // return the last item of the array
+  }
+  // 3
+  function getArrayLength(arr) {
+    // return the length of the array
+  }
+  // 4
+  function incrementByOne(arr) {
+    // arr is an array of integers  
+    // increase each integer by one
+    // return the array
+  }
+  // 5
+  function addItemToArray(arr, item) {
+    // add the item to the end of the array
+    // return the array
+  }
+  // 6
+  function addItemToFront(arr, item) {
+    // add the item to the front of the array
+    // return the array
+    // hint: use the array method .unshift
+  }
+  // 7
+  function wordsToSentence(words) {
+    // words is an array of strings
+    // return a string that is all of the words concatenated together
+    // spaces need to be between each word
+    // example: ['Hello', 'world!'] -> 'Hello world!'
+  }
+  // 8
+  function contains(arr, item) {
+    // check to see if item is inside of arr
+    // return true if it is, otherwise return false
+  }
+  // 9
+  function addNumbers(numbers) {
+    // numbers is an array of integers.
+    // add all of the integers and return the value
+  }
+  // 10
+  function averageTestScore(testScores) {
+    // testScores is an array.  Iterate over testScores and compute the average.
+    // return the average
+  }
+  // 11
+  function largestNumber(numbers) {
+    // numbers is an array of integers
+    // return the largest integer
+  }
+  // 12
+  function multiplyArguments() {
+    // use the arguments keyword to multiply all of the arguments together and return the product
+    // if no arguments are passed in return 0
+    // if one argument is passed in just return it
+  }
+  
+  // Do not modify code below this line.
+  // --------------------------------
+  
+  module.exports = {
+    returnFirst,
+    returnLast,
+    getArrayLength,
+    incrementByOne,
+    addItemToArray,
+    addItemToFront,
+    wordsToSentence,
+    contains,
+    addNumbers,
+    averageTestScore,
+    largestNumber,
+    multiplyArguments,
+  };
+  
